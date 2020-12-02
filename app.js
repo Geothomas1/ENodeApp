dbeac8c274b81f6a32413dad7775fd906bd86f8b
geothomas <geothomas@cet.ac.in>
2020-12-02 23:01:08 +0530

    view product in admin and user pages

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var hbs = require('express-handlebars')
