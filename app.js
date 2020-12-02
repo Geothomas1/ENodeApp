7796dc4a22808a77bd236a3fdfeda7a45df049ce
geothomas <geothomas@cet.ac.in>
2020-12-02 07:32:22 +0530

    Database connection added in config/connection.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var adminRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
 var app = express();
 var fileUpload=require('express-fileupload');
+var db=require('./config/connection')
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//use db connect app.use before root connection 
+app.connect((err)=>{
+  if(err)
+  console.log("Connection Error"+err)
+  else
+  console.log("Database connected.!")
+
+
+})
 app.use(fileUpload());
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
