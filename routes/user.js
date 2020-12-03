2c6e54de774e1154eeda4e0ddbdfc5bc79da0123
geothomas <geothomas@cet.ac.in>
2020-12-03 19:32:41 +0530

    bcrypt moduel  added

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
 var productHelper=require('../helpers/product-helper')
-
+var userHelper=require('../helpers/user-helpers')
 /* GET home page. */
 router.get('/', function (req, res, next) {
   productHelper.getAllProducts().then((products)=>{
@@ -18,7 +19,10 @@ router.get('/signup',(req,res)=>{
   res.render('user/signup')
 })
 router.post('/signup',(req,res)=>{
-  
+  userHelper.doSignup(req.body).then((response)=>{
+    console.log(response)
+  })
+
 })
 
 module.exports = router;
