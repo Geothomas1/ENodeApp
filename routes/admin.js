756959cea20052992596d27a610010b0a1f930b0
geothomas <geothomas@cet.ac.in>
2020-12-05 17:43:24 +0530

    image upload and update success

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ var productHelper = require('../helpers/product-helper');
 const { response } = require('../app');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  productHelper.getAllProducts().then((products)=>{
+  productHelper.getAllProducts().then((products) => {
     //console.log(products)
     res.render('admin/view-products', { admin: true, products })
 
@@ -39,24 +39,29 @@ to get the value here take it as !
 let productId=req.query.id
 
 */
-router.get('/delete-product/:id',(req,res)=>{
-  let productId=req.params.id
+router.get('/delete-product/:id', (req, res) => {
+  let productId = req.params.id
   console.log(productId)
-  productHelper.deleteProduct(productId).then((response)=>{
+  productHelper.deleteProduct(productId).then((response) => {
     res.redirect('/admin/')
   })
 
 })
 
-router.get('/edit-product/:id',async(req,res)=>{
-  let product=await productHelper.getProductDetails(req.params.id)
+router.get('/edit-product/:id', async (req, res) => {
+  let product = await productHelper.getProductDetails(req.params.id)
   console.log(product)
-  res.render('./admin/edit-product',{product})
+  res.render('./admin/edit-product', { product })
 })
-router.post('/edit-product/:id',(req,res)=>{
-  console.log(req.params.id)
-  productHelper.updateProduct(req.params.id,req.body).then(()=>{
+router.post('/edit-product/:id', (req, res) => {
+  //console.log(req.params.id)
+  let id=req.params.id
+  productHelper.updateProduct(req.params.id, req.body).then(() => {
     res.redirect('/admin')
+    if (req.files.image) {
+      let image=req.files.image
+      image.mv('./public/product-images/' + id + '.jpeg')
+    }
   })
 
 })
